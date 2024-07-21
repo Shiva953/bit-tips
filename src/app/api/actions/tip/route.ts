b24c869b41d17ef142f08a00a820325eb13ba4b0
Shiva953 <b22070@students.iitmandi.ac.in>
2024-07-21 22:04:42 +0530

    setup blink+action

---
+++ src/app/api/actions/tip/route.ts
@@ -0,0 +1,205 @@
+/**
+ * Solana Actions Example
+ */
+
+import {
+    ActionPostResponse,
+    ACTIONS_CORS_HEADERS,
+    createPostResponse,
+    ActionGetResponse,
+    ActionPostRequest,
+  } from "@solana/actions";
+  import {
+    Authorized,
+    clusterApiUrl,
+    Connection,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    StakeProgram,
+    SystemProgram,
+    Transaction,
+  } from "@solana/web3.js";
+
+  export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+    "5SVgMJHrMLYGfPw9nYpPuH6DDWpbNAbs5Dm7kyyREhRR",
+  );
+  
+  export const DEFAULT_SOL_AMOUNT: number = 1.0;
+  
+  //GET req -> returns the action metadeta as response(which would be unfurled into a blink by supported client)
+  export const GET = async (req: Request) => {
+    try {
+      const requestUrl = new URL(req.url);
+      const { toPubkey } = validatedQueryParams(requestUrl);
+  
+      const baseHref = new URL(
+        `/api/actions/tip?to=${toPubkey.toBase58()}`,
+        requestUrl.origin,
+      ).toString();
+  
+      const payload: ActionGetResponse = {
+        title: "Bit-Tips",
+        icon: new URL("https://cdn.prod.website-files.com/6531cf988b06260fbdbb0235/65497a45e8180f1d955675a5_653eb80086c2be667c2793e1_653eb14f23b4c738f6353388_3356.png", requestUrl.origin).toString(),
+        description: "Tip your favourite devs, writers and artists!",
+        label: "Transfer", // this value will be ignored since `links.actions` exists
+        links: {
+          actions: [
+            {
+              label: "Send 0.1 SOL", // button text
+              href: `${baseHref}&amount=${"0.1"}`,
+            },
+            {
+              label: "Send 0.5 SOL", // button text
+              href: `${baseHref}&amount=${"0.5"}`,
+            },
+            {
+              label: "Send 1 SOL", // button text
+              href: `${baseHref}&amount=${"1"}`,
+            },
+            {
+              label: "Send SOL", // button text
+              href: `/api/actions/tip?to={recipient}&amount={amount}`, // this href will have a text input
+              parameters: [
+                {
+                  name: "amount", // parameter name in the `href` above
+                  label: "Amount(in SOL)", // placeholder of the text input
+                  required: true,
+                },
+                {
+                    name: "recipient", // parameter name in the `href` above
+                    label: "Wallet Address to be tipped", // placeholder of the text input
+                    required: true,
+                },
+              ],
+            },
+          ],
+        },
+      };
+  
+      return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    } catch (err) {
+      console.log(err);
+      let message = "An unknown error occurred";
+      if (typeof err == "string") message = err;
+      return new Response(message, {
+        status: 400,
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    }
+  };
+  
+  //take the input amount + pubkey, make a POST req with those params to the actions server
+  // actions server returns a base64 encoded TXN, which shall be signed by the user on the client side with his wallet
+  export const POST = async (req: Request) => {
+    try {
+      const requestUrl = new URL(req.url);
+      const { amount, toPubkey } = validatedQueryParams(requestUrl); //decoding query params
+  
+      const body: ActionPostRequest = await req.json(); //the POST request body
+  
+      // validate the client provided input
+      let account: PublicKey;
+      try {
+        account = new PublicKey(body.account);
+      } catch (err) {
+        return new Response('Invalid "account" provided', {
+          status: 400,
+          headers: ACTIONS_CORS_HEADERS,
+        });
+      }
+  
+      const connection = new Connection(
+        process.env.SOLANA_RPC! || clusterApiUrl("devnet"), "confirmed"
+      );
+  
+      
+      const minimumBalance = await connection.getMinimumBalanceForRentExemption(
+        0, 
+      );
+      if (amount * LAMPORTS_PER_SOL < minimumBalance) {
+        throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
+      }
+  
+      const transaction = new Transaction();
+  
+      transaction.add(
+        SystemProgram.transfer({
+          fromPubkey: account,
+          toPubkey: toPubkey,
+          lamports: amount * LAMPORTS_PER_SOL,
+        }),
+      );
+  
+      // set the end user as the fee payer
+      transaction.feePayer = account;
+  
+      transaction.recentBlockhash = (
+        await connection.getLatestBlockhash()
+      ).blockhash;
+  
+      // response for the POST req is in this format
+      // it signs the txn, base64 encodes it, POST response form => {base64 encoded txn, message}
+      const payload: ActionPostResponse = await createPostResponse({
+        fields: {
+          transaction,
+          message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
+        },
+        // note: no additional signers are needed
+        // signers: [],
+      }); 
+  
+      return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    } catch (err) {
+        console.log(err);
+        let message = "An unknown error occurred";
+        if (typeof err == "string") message = err;
+        return new Response(message, {
+            status: 400,
+            headers: ACTIONS_CORS_HEADERS,
+        });
+    }
+  };
+
+  // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+  // THIS WILL ENSURE CORS WORKS FOR BLINKS
+  //   export const OPTIONS = [GET, POST];
+  export const OPTIONS = async (req: Request) => {
+    return new Response(null, {
+      status: 204,
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  };
+
+  // req url -> {amount, pubkey}
+  function validatedQueryParams(requestUrl: URL) {
+    let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
+    let amount: number = DEFAULT_SOL_AMOUNT;
+  
+    try {
+      if (requestUrl.searchParams.get("to")) {
+        toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
+      }
+    } catch (err) {
+      throw "Invalid input query parameter: to";
+    }
+  
+    try {
+      if (requestUrl.searchParams.get("amount")) {
+        amount = parseFloat(requestUrl.searchParams.get("amount")!);
+      }
+  
+      if (amount <= 0) throw "amount is too small";
+    } catch (err) {
+      throw "Invalid input query parameter: amount";
+    }
+  
+    return {
+      amount,
+      toPubkey,
+    };
+  }
---
+++ src/components/ManyActions.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import '@dialectlabs/blinks/index.css';
+import { useState, useEffect, useMemo } from 'react';
+import { Action, Blink, ActionsRegistry, type ActionAdapter } from "@dialectlabs/blinks";
+import { useAction, useActionsRegistryInterval } from '@dialectlabs/blinks/react';
+
+export const ManyActions = ({ adapter }: { adapter: ActionAdapter }) => {
+    const apiUrls = useMemo(() => (['http://localhost:3000/api/actions/tip']), []);
+    const [actions, setActions] = useState<Action[]>([]);
+    
+    useEffect(() => {
+        const fetchActions = async () => {
+            const promises = apiUrls.map(url => Action.fetch(url).catch(() => null));
+            const actions = await Promise.all(promises);
+            
+            setActions(actions.filter(Boolean) as Action[]);
+        }
+        
+        fetchActions();
+    }, [apiUrls]);
+    
+    // we need to update the adapter every time, since it's dependent on wallet and walletModal states
+    useEffect(() => {
+        actions.forEach((action) => action.setAdapter(adapter));
+    }, [actions, adapter]);
+    
+    return actions.map(action => (
+        <div key={action.url} className="flex flex-row gap-2 w-[28rem] h-96 mx-auto">
+            <Blink stylePreset="x-dark" action={action} websiteText={new URL(action.url).hostname} />
+        </div>
+    ))
+}
